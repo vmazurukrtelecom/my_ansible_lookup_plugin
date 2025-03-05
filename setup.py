a12f47cd5c7659524eac92470c12051ec1a13441
vmazurukrtelecom <155393582+vmazurukrtelecom@users.noreply.github.com>
2025-03-05 17:32:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ setup(
     description='Custom Ansible lookup plugin for AWX',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/yourusername/my_ansible_lookup_plugin',
+    author='Volodymyr Mazur',
+    author_email='vmazur@ukrtelecom.ua',
+    url='https://github.com/vmazurukrtelecom/my_ansible_lookup_plugin',
     packages=find_packages(),
+    package_data={
+        '': ['lookup_plugins/*.py'],  # Включаємо всі .py файли з lookup_plugins
+    },
     include_package_data=True,
-    install_requires=[],  # Додайте залежності, якщо є (наприклад, з requirements.txt)
+    install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
